77775d3d7f0203f46fab213ad590c219e6771c81
DreamingLri <3507415455@qq.com>
2023-10-20 17:52:17 +0800

    add swagger3

--- src/main/java/com/project/EasyAreaCodesApplication.java
+++ src/main/java/com/project/EasyAreaCodesApplication.java
@@ -1,8 +1,12 @@
 package com.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@MapperScan("com.project.mapper")
+@EnableOpenApi
 @SpringBootApplication
 public class EasyAreaCodesApplication {
 
