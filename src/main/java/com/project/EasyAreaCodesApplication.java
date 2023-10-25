297a21e572d4ce68fe48a9a67d66e911a865093a
DreamingLri <3507415455@qq.com>
2023-10-25 11:36:17 +0800

    update to java21 with springboot3 remove swagger3 add springdoc

--- src/main/java/com/project/EasyAreaCodesApplication.java
+++ src/main/java/com/project/EasyAreaCodesApplication.java
@@ -3,15 +3,13 @@ package com.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
-@MapperScan("com.project.mapper")
-@EnableOpenApi
 @SpringBootApplication
+@MapperScan("com.project.mapper")
 public class EasyAreaCodesApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EasyAreaCodesApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EasyAreaCodesApplication.class, args);
+	}
 
 }
--- src/test/java/com/project/EasyAreaCodesApplicationTests.java
+++ src/test/java/com/project/EasyAreaCodesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EasyAreaCodesApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
